a581c090fe2e006491bd0603c87c53e846df4514
Lambels <patrick.arvatu@yahoo.com>
2023-03-28 18:04:20 +0300

    added explanation for invariance

--- exercises/standard_library_types/cow1.rs
+++ exercises/standard_library_types/cow1.rs
@@ -10,7 +10,29 @@
 use std::borrow::Cow;
 
 // use 2 lifetimes to allow the compiler to monomorphise or replace them independently. This is
-// because &mut T is invariant over
+// because &mut T is invariant over T and borrowing the Cow smart pointer in the following way wont
+// work:
+//
+// let mut v = ... (Cow)
+// abs_all(&mut v);
+// abs_all(&mut v);
+// println!("{:?}", v);
+//
+// since v implicitly has to live till the last call to println, so if we were to use one single
+// lifetime we would be forced to draw the following conections:
+// abs_all<'a>(input: &'a mut Cow<'a, ...>) -> &'a mut Cow<'a, ...>
+//
+// now you can see that the mutable reference will live for as long as the Cow<'a, ...> smart
+// pointer lives. This is problamatic because later if we want to use v, like in the second abs_all
+// call we would run in a conflict of 2 mutable references being passed out which is rightfully
+// caught by the borrow checker.
+//
+// This wouldnt be a problem if we were to use immutable references because &T is covariant in T
+// and the compiler would be able to shorten the lifetime of the inner borrow automatically to make
+// our code compile. This isnt the case with mutable references because the compiler cant shorten
+// the lifetime or replace T with a subtype because &mut T is invariant over T.
+//
+// This is fixed by using 2 different lifetimes which get monomorphised separately.
 fn abs_all<'a, 'b>(input: &'a mut Cow<'b, [i32]>) -> &'a mut Cow<'b, [i32]> {
     for i in 0..input.len() {
         let v = input[i];
